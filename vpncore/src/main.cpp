cd3d76a2b19ade1798263951d6756de90e1665c8
jack <jack.wgm@gmail.com>
2018-08-08 13:12:16 +0800

    remove unused function in_cksum

--- vpncore/src/main.cpp
+++ vpncore/src/main.cpp
@@ -1217,36 +1217,6 @@ private:
 		return false;
 	}
 
-	unsigned short in_cksum(unsigned short *addr, int len)
-	{
-		int				nleft = len;
-		int				sum = 0;
-		unsigned short	*w = addr;
-		unsigned short	answer = 0;
-
-		/*
-		 * Our algorithm is simple, using a 32 bit accumulator (sum), we add
-		 * sequential 16 bit words to it, and at the end, fold back all the
-		 * carry bits from the top 16 bits into the lower 16 bits.
-		 */
-		while (nleft > 1)  {
-			sum += *w++;
-			nleft -= 2;
-		}
-
-		/* 4mop up an odd byte, if necessary */
-		if (nleft == 1) {
-			*(unsigned char *)(&answer) = *(unsigned char *)w ;
-			sum += answer;
-		}
-
-		/* 4add back carry outs from top 16 bits to low 16 bits */
-		sum = (sum >> 16) + (sum & 0xffff);	/* add hi 16 to low 16 */
-		sum += (sum >> 16);			/* add carry */
-		answer = ~sum;				/* truncate to 16 bits */
-		return(answer);
-	}
-
 
 private:
 	boost::asio::io_context& m_io_context;
