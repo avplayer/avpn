c960de8d000da105859419575d9479196bcac61d
jack <jack.wgm@gmail.com>
2018-08-10 01:40:13 +0800

    update tuntap.cpp

--- vpncore/src/tuntap.cpp
+++ vpncore/src/tuntap.cpp
@@ -1,6 +1,19 @@
+#ifdef AVPN_WINDOWS
 #include "tuntap_windows_service.hpp"
+#endif
+
+#ifdef AVPN_LINUX
+#include "tuntap_fd_service.hpp"
+#endif
 
 namespace tuntap_service {
+
+#ifdef AVPN_WINDOWS
 	boost::asio::io_context::id tuntap_windows_service::id;
+#endif // AVPN_WIN
+
+#ifdef AVPN_LINUX
+	boost::asio::io_context::id tuntap_fd_service::id;
+#endif
 } // namespace tuntap_service
 
