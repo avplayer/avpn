83b8c659331872918fa70559c3667cbe796beb69
jack <jack.wgm@gmail.com>
2018-08-28 05:56:12 +0800

    add vpn_keys.hpp

---
+++ vpncore/include/vpn/vpn_keys.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "crypto/xchacha20poly1305_crypto.hpp"
+
+namespace avpncore {
+	using vpn_keys = std::vector<crypto::xchacha20poly1305_key>;
+
+	inline vpn_keys load_vpn_keys(const std::string& file)
+	{
+		vpn_keys keys;
+		std::ifstream ifs(file, std::ifstream::in);
+		std::string line;
+
+		while (ifs.good())
+		{
+			std::getline(ifs, line);
+			if (line.empty())
+				continue;
+			keys.push_back(line);
+		}
+
+		return keys;
+	}
+}
